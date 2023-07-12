2587d1b62409bc68b264f3dc694fc6e63413291c
Yana <yanahomiak99@gmail.com>
2023-07-12 20:40:35 +0300

    feat(DAH-6): create error page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavbarTitle } from "./styled";
+
+const ErrorPage = () => {
+    return (
+        <NavbarTitle>
+            <h2>Error</h2>
+        </NavbarTitle>
+    );
+};
+export default ErrorPage;
